cb1cc122f263f637bda9137a851dd641bde0a9db
naruto33ee44rr-lgtm <naruto33ee44rr@gmail.com>
2026-09-14 23:29:13 +0300

    Update Quran.py

--- Quran.py
+++ Quran.py
@@ -64,7 +64,8 @@ ID_BOT_OWNER_EMOJI = "6158862632926319619"  # إيموجي Bot Owner
 ID_DEV_USERNAME_EMOJI = "6269163801178804220"  # إيموجي اليوزر
 ID_NAV_NEXT = "5291842193382730408"  # التالي
 ID_NAV_PREV = "5291993277447301940"  # السابق
-ID_RECITER_EMOJI = "6071346841704733378"  # القارئ
+ID_RECITER_EMOJI = "6071346841704733378"  # زر اختر القارئ
+ID_RECITER_OPTION_EMOJI = "6269163801178804220"  # إيموجي خيارات اختيار القارئ
 ID_MODE_BTN = "5355082606109013423"  # إيموجي أزرار الاختيار
 ID_WAITING = "5355226302829835543"  # إيموجي الانتظار
 ID_FUTURE_SECTION = "5355295550587549286"  # إيموجي الأقسام المستقبلية
@@ -100,31 +101,23 @@ EMOJI_ALERT = f'<tg-emoji emoji-id="{ID_ALERT}">⚠️</tg-emoji>'
 EMOJI_CAPTION_HTML = f'<tg-emoji emoji-id="{ID_PAGE_CAPTION}">📖</tg-emoji>'
 EMOJI_SECURITY_PANEL = f'<tg-emoji emoji-id="{ID_SECURITY_PANEL}">🛡️</tg-emoji>'
 
-# قائمة القراء المتاحين
+# قائمة القراء المعتمدة بعد الإصلاح
 RECITERS = {
     "dussary": {
         "name": "د. ياسر الدوسري",
         "url": "https://everyayah.com/data/Yasser_Ad-Dussary_128kbps",
     },
-    "abdulbasit": {
-        "name": "عبد الباسط عبد الصمد",
-        "url": "https://everyayah.com/data/Abdul_Basit_Murattal_128kbps",
-    },
-    "alafasy": {
-        "name": "مشاري العفاسي",
-        "url": "https://everyayah.com/data/Alafasy_128kbps",
+    "minshawy": {
+        "name": "محمد صديق المنشاوي",
+        "url": "https://everyayah.com/data/Minshawy_Murattal_128kbps",
     },
     "maher": {
         "name": "ماهر المعيقلي",
         "url": "https://everyayah.com/data/MaherAlMuaiqly128kbps",
     },
-    "sudais": {
-        "name": "عبد الرحمن السديس",
-        "url": "https://everyayah.com/data/Abdurrahmaan_As-Sudais_128kbps",
-    },
-    "minshawy": {
-        "name": "محمد صديق المنشاوي",
-        "url": "https://everyayah.com/data/Minshawy_Murattal_128kbps",
+    "alafasy": {
+        "name": "مشاري العفاسي",
+        "url": "https://everyayah.com/data/Alafasy_128kbps",
     },
 }
 
@@ -675,7 +668,7 @@ def build_reciter_select_menu(user_id: int) -> InlineKeyboardMarkup:
                 text=f"{prefix}{info['name']}",
                 callback_data=f"set_reciter:{key}",
                 style=button_style,
-                icon_custom_emoji_id=ID_RECITER_EMOJI,
+                icon_custom_emoji_id=ID_RECITER_OPTION_EMOJI,
             )
         ])
 
